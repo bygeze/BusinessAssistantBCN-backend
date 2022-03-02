59532007f96d83488277e0f7c57955a761d501a8
laura <laurabf6@gmail.com>
2022-03-02 13:55:04 +0100

    tests package & dependencies: mockito-core, spring-test,spring-boot-starter-test

---
+++ BusinessAssistant-mydata/src/test/java/com/businessassistantbcn/mydata/service/UserServiceTest.java
@@ -0,0 +1,34 @@
+package com.businessassistantbcn.mydata.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UserServiceTest {
+
+//	@Test
+//	void testUserService() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	void testSaveSearch() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	void testGetAllSearches() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	void testGetSearchResults() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	void testFilterJsonNode() {
+//		fail("Not yet implemented");
+//	}
+
+}
